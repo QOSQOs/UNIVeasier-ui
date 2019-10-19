57ccf6133074421f3f645fcf2be5f34b50884ef0
Edison Kleiber Ttito <Kleiber100892@gmail.com>
2019-10-19 12:46:12 -0300

    feat: add and list operation using test model (#2)

---
+++ src/components/FormTest.vue
@@ -0,0 +1,64 @@
+<!-- Add HTML -->
+<template>
+  <div>
+    <p>
+      <label>Name</label>
+      <input v-model="name">
+    </p>
+    
+    <p>
+      <label>Age</label>
+      <input v-model.number="age">
+    </p>
+
+    <p>
+      <label>Email</label>
+      <input v-model="email">
+    </p>
+    <button v-on:click="AddTest()">Add</button>
+  </div>
+</template>
+
+
+<!-- Add JS code -->
+<script>
+import axios from 'axios'
+
+export default {
+    data(){
+      return {
+        name: null,
+        age: null,
+        email: null
+      }
+    },
+    methods: {
+      clearData(){
+        this.name = null
+        this.age = null
+        this.email = null
+      },
+      AddTest(){
+        axios.post('/test', {
+          name: this.name,
+          age: this.age,
+          email: this.email
+        })
+        .then(response => {
+          console.log(response.data)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+
+        this.clearData()
+      }
+    }
+}
+</script>
+
+
+<!-- Add CSS -->
+<style>
+
+</style>
---
+++ src/components/TableTest.vue
@@ -0,0 +1,52 @@
+<!-- Add HTML -->
+<template>
+  <div>
+    <button v-on:click="getListTest()">List</button>
+    <tr>
+      <th>ID</th>
+      <th>NAME</th>
+      <th>AGE</th>
+      <th>EMAIL</th>
+    </tr>
+    <tr v-for="test in tests" :key="test.id">
+      <td>{{test.id}}</td>
+      <td>{{test.name}}</td>
+      <td>{{test.age}}</td>
+      <td>{{test.email}}</td>
+    </tr>
+  </div>
+</template>
+
+
+<!-- Add JS code -->
+<script>
+import axios from 'axios'
+
+export default {
+  data(){
+    return {
+      tests: null
+    }
+  },
+  mounted() {
+    this.getListTest()
+  },
+  methods: {
+    getListTest() {
+      axios.get('/test')
+      .then( response => {
+        this.tests = response.data
+      })
+      .catch( error => {
+        console.log(error)
+      })
+    }
+  }
+}
+</script>
+
+
+<!-- Add CSS -->
+<style>
+
+</style>
